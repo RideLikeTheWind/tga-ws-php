cb2440c5bea8ad649af2a1b9b47a44cb704670f9
Joe <RideLikeTheWind@users.noreply.github.com>
2014-07-03 21:46:09 +1000

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,25 @@
+/**
+* With thanks to Michael Haberle for this snippet
+* http://ageekandhisblog.com/php-how-to-convert-stdclass-object-to-array/
+**/
+
+function objectToArray($d) {
+		if (is_object($d)) {
+			// Gets the properties of the given object
+			// with get_object_vars function
+			$d = get_object_vars($d);
+		}
+ 
+		if (is_array($d)) {
+			/*
+			* Return array converted to object
+			* Using __FUNCTION__ (Magic constant)
+			* for recursive call
+			*/
+			return array_map(__FUNCTION__, $d);
+		}
+		else {
+			// Return array
+			return $d;
+		}
+	}
